debd12fe35a883480dcd8eff0cc017a245baaa12
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-05-26 15:15:27 +0900

    [doce] Function Interface, Introduction to Lambda 2

--- src/main/java/me/whiteship/java8to11/Foo.java
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -5,13 +5,30 @@ public class Foo {
     // 자바 8 이전
     public static void main(String[] args) {
         
-        // 익명 내부 클래스 (anonymous inner class)
-        RunSomething runSomething = new RunSomething() {
+        // 익명 내부 클래스 (anonymous inner class), 자바 8이전
+        RunSomething runSomething1 = new RunSomething() {
             @Override
             public void doIt() {
-
+                System.out.println("Hello");
             }
         };
+
+        RunSomething runSomething2 = () -> System.out.println("Hello");
+
+        RunSomething runSomething3 = Foo::doIt;
+
+        RunSomething runSomething4 = () -> {
+            System.out.println("Hello");
+            System.out.printf("Hong");
+        };
+
+        runSomething1.doIt();
+        runSomething2.doIt();
+        runSomething3.doIt();
+        runSomething4.doIt();
     }
 
+    private static void doIt() {
+        System.out.println("Hello");
+    }
 }
--- src/main/java/me/whiteship/java8to11/RunSomething.java
+++ src/main/java/me/whiteship/java8to11/RunSomething.java
@@ -1,6 +1,6 @@
 package me.whiteship.java8to11;
 
-@FunctionalInterface
+@FunctionalInterface // java standard lib에 들어있어서 별도의 import가 되지 않는다.
 public interface RunSomething {
 
     void doIt();
@@ -10,7 +10,7 @@ public interface RunSomething {
     }
 
     default void printAge() {
-        System.out.println("40");
+        System.out.println("25");
     }
 
 }
